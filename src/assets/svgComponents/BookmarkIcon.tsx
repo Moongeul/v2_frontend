55a693a16c94f4126ea11645f664f0a22c71010f
유림 <yulimh001@gmail.com>
2025-06-17 15:31:09 +0900

    feat: 아이콘 추가

---
+++ src/assets/svgComponents/BookmarkIcon.tsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgBookmarkIcon = (props: SVGProps<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 14 16" {...props}><path fill="#FF6E1F" d="m7 14.02-4.268 1.843q-.804.331-1.511-.139t-.707-1.316V0h12.973v14.408q0 .846-.707 1.316t-1.51.139z" /></svg>;
+export default SvgBookmarkIcon;
