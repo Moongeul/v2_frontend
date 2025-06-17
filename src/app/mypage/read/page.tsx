6921a544d4afb0098b09cc348ec665be05ba0dad
유림 <yulimh001@gmail.com>
2025-06-17 17:45:28 +0900

    style: 내가 읽은 책 퍼블리싱

--- src/app/mypage/read/page.tsx
+++ src/app/mypage/read/page.tsx
@@ -1,3 +1,58 @@
+'use client'
+
+import Header from '@/components/common/Header'
+import Button from '@/components/common/Button'
+import DropBox from '@/components/common/DropBox'
+import { useState } from 'react'
+import BookshelfItem from '@/components/mypage/read/BookshelfItem'
+
 export default function Read() {
-  return <></>
+  const [filter, setFilter] = useState<string>('전체 보기')
+  const filterContents = ['전체 보기', '월별 보기', '평점 높은 순', '평점 낮은 순']
+  const [isFilterClicked, setIsFilterClicked] = useState(false)
+  return (
+    <main>
+      <Header
+        headerType={'DYNAMIC'}
+        rightComponent={
+          <Button customClassName={'w-[48px]'} size={'large'} state={'main'}>
+            추가
+          </Button>
+        }
+      >
+        내가 읽은 책
+      </Header>
+      <div className="mt-[120px] px-5">
+        <DropBox selectedContent={filter} isFilterClicked={isFilterClicked} setIsFilterClicked={setIsFilterClicked}>
+          <>
+            {filterContents.map((content) => {
+              return (
+                <div
+                  key={content}
+                  className="title-regular-14 py-2"
+                  onClick={() => {
+                    setFilter(content)
+                    setIsFilterClicked(false)
+                  }}
+                >
+                  {content}
+                </div>
+              )
+            })}
+          </>
+        </DropBox>
+        <section className="mt-4 grid grid-cols-3 gap-4">
+          <BookshelfItem />
+          <BookshelfItem />
+          <BookshelfItem />
+          <BookshelfItem />
+          <BookshelfItem />
+          <BookshelfItem />
+          <BookshelfItem />
+          <BookshelfItem />
+          <BookshelfItem />
+        </section>
+      </div>
+    </main>
+  )
 }
---
+++ src/components/mypage/read/BookshelfItem.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import { KidStarIcon } from '@/assets/svgComponents'
+
+interface BookshelfItemProps {}
+export default function BookshelfItem({}: BookshelfItemProps) {
+  return (
+    <div className="flex flex-col items-center justify-center gap-y-2 border-b p-2">
+      <Image src="/book.png" width={76} height={115} className={'mx-1'} alt={'book'} />
+      <section className="flex h-[92px] w-full flex-col gap-y-1">
+        <div className="flex items-center gap-x-1">
+          <KidStarIcon width={16} height={16} />
+          <p className="title-medium-14 text-dark-gray">{'4.5'}</p>
+        </div>
+        <div className="title-medium-14 line-clamp-2 h-[44px]">{'나미야 잡화점의 기적'}</div>
+        <p className="caption-1 text-dark-gray">{'2024.05.12'}</p>
+      </section>
+    </div>
+  )
+}
